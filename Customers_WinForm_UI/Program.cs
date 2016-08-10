7368694a368e02f2d5bb47865718206a84817ea6
slawe <slawe@DESKTOP-7T1O0LH>
2016-08-10 23:42:29 +0200

    ...

--- Customers_WinForm_UI/Program.cs
+++ Customers_WinForm_UI/Program.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 namespace Customers_WinForm_UI
 {
     /// <summary>
-    /// Main program class (test)
+    /// Main program class
     /// </summary>
     static class Program
     {
